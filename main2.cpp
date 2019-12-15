d414bef50044a33e61c2c5524b546fe70ab5c376
sdsd08013 <sd08013+git@gmail.com>
2019-12-16 02:35:23 +0900

    write xml

--- main2.cpp
+++ main2.cpp
@@ -12,23 +12,43 @@
 #include "SkColorSpace.h"
 
 #include "transform.h"
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/foreach.hpp>
+#include <boost/lexical_cast.hpp>
 
+using namespace boost::property_tree;
+using namespace std;
 int main() {
-  //cout << SkColorGetR(SkColorSetARGB(0x80, 0xFF, 0xFF, 0x00)) << endl;
 
-  /*
-  std::cout << SkColorGetR(SkColorSetARGB(0x80, 0xFF, 0xFF, 0x00)) << std::endl;
-  std::cout << SK_ColorBLUE << std::endl;
-  std::cout << android::uirenderer::Color::Red_500 << std::endl;
-  */
+  int count = 0;
+  ptree npt;
+  ptree& root = npt.put("resources", "");
+  ptree pt;
+  read_xml("colors.xml", pt);
 
-  SkColor b = SK_ColorBLUE;
+  BOOST_FOREACH (const ptree::value_type& child, pt.get_child("resources")) {
+      const string name = child.second.get<string>("<xmlattr>.name");
 
-  //SkColorSpace::MakeSRGB();
-  //android::uirenderer::sRGBToLab(SK_ColorBLUE);
-  SkColor dc = android::uirenderer::makeDark(b);
-  //android::ColorSpace::test();
-  std::cout << SkColorGetA(dc) << std::endl;
+      const string l = boost::lexical_cast<string>(child.second.data());
+      const char* const rs = l.substr(1,2).c_str();
+      const char* const gs = l.substr(3,2).c_str();
+      const char* const bs = l.substr(5,2).c_str();
+
+      double r = strtol(rs, NULL, 16);
+      double g = strtol(bs, NULL, 16);
+      double b = strtol(gs, NULL, 16);
+
+      string nr = to_string(SkColorGetR(android::uirenderer::makeDark(SkColorSetRGB(r, g, b))));
+      string ng = to_string(SkColorGetG(android::uirenderer::makeDark(SkColorSetRGB(r, g, b))));
+      string nb = to_string(SkColorGetB(android::uirenderer::makeDark(SkColorSetRGB(r, g, b))));
+
+      ptree& color = root.add("color", "#" + nr + ng + nb);
+      color.put("<xmlattr>.name", name);
+
+      count++;
+  }
+  write_xml("dark_colors.xml", npt, std::locale(),xml_writer_make_settings<std::string>(' ', count));
 
   return 0;
  
